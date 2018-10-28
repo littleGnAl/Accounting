a012a5332399162bb216f58117ae7d1d76367afa
littleGnAl <tearjeaker@hotmail.com>
2018-10-28 18:11:47 +0800

    Do some upgrade

--- app/src/main/java/com/littlegnal/accounting/base/util/DimensionExtensions.kt
+++ app/src/main/java/com/littlegnal/accounting/base/util/DimensionExtensions.kt
@@ -5,17 +5,17 @@ import android.view.View
 
 /** Copy from [anko](https://github.com/Kotlin/anko/blob/d5a526512b48c5cd2e3b8f6ff14b153c2337aa22/anko/library/static/commons/src/Dimensions.kt)
  */
-//returns dip(dp) dimension value in pixels
+// returns dip(dp) dimension value in pixels
 fun Context.dip(value: Int): Int = (value * resources.displayMetrics.density).toInt()
 
 fun Context.dip(value: Float): Int = (value * resources.displayMetrics.density).toInt()
 
-//return sp dimension value in pixels
+// return sp dimension value in pixels
 fun Context.sp(value: Int): Int = (value * resources.displayMetrics.scaledDensity).toInt()
 
 fun Context.sp(value: Float): Int = (value * resources.displayMetrics.scaledDensity).toInt()
 
-//the same for the views
+// the same for the views
 fun View.dip(value: Int): Int = context.dip(value)
 
 fun View.dip(value: Float): Int = context.dip(value)
